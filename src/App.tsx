8964bde503bd5be1b3990f253beadbf4238855be
Maxime PIGEON <maxpige@gmail.com>
2024-06-05 20:30:41 +0200

    cleanup and add login logout to pods

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { SolidForm } from "./SolidForm";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col w-full  items-center p-10 gap-4">
+      <div className="flex gap-4 items-center border-4 border-yellow-300 p-8 rounded-xl shadow-xl">
+        <img src="/blue-carrot.png" alt="carrot" className="size-10" />
+        <h1 className="text-3xl font-bold text-blue-700">Blue Carrot</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <SolidForm />
+    </div>
+  );
 }
 
-export default App
+export default App;
